072a64105a61d90cacce9db762321a395b7a516e
Mati ul Rehman <mati.rehman.dev@gmail.com>
2025-09-30 20:07:12 +0500

    added

--- js/dom.js
+++ js/dom.js
@@ -18,6 +18,8 @@
 // console.log(bd.childNodes);
 // console.log(bd.children);
 // console.log(bd);
+// const ArrayChild=Array.from(bd.children);
+// ArrayChild[4].style.backgroundColor="Blue";
 
 // bd.style.display="none";
 
@@ -27,14 +29,47 @@
 //     console.log(d);
 // });
 
-const list = document.getElementsByClassName("list-item");
-console.log(list);
+// const list = document.getElementsByClassName("list-item");
+// console.log(list);
 
-const listArr = Array.from(list);
-listArr.forEach((l, i) => {
-    l.innerText = `item ${i + 1}`;
-    l.style.backgroundColor = "coral";
-    l.style.color = "beige";
-});
+// const listArr = Array.from(list);
+// listArr.forEach((l, i) => {
+//     l.innerText = `item ${i + 1}`;
+//     l.style.backgroundColor = "coral";
+//     l.style.color = "beige";
+// });
 
 
+// const elements = document.querySelectorAll('li');
+// let index=0;
+// elements.forEach((e)=>{
+// if(e.innerText==="Banana"){
+//     index = Array.from(elements).indexOf(e);
+//     return;
+// }
+// })
+
+// console.log(elements.innerText);
+
+// elements[index].style.backgroundColor="yellow";
+// console.log(window);
+// console.log(document);
+// console.log(history);
+// console.log(navigator);
+// console.log(screen);
+
+
+const list = document.getElementById("myList");
+const child=document.querySelector("li:nth-child(1)");
+child.textContent="Pineapple";
+
+const newLi= document.querySelectorAll("li");
+
+const newItem = document.createElement("li");
+console.log(newItem.nextSibling);
+newItem.textContent = "Mango";
+newItem.style.backgroundColor="yellow";
+newItem.style.color="white";
+list.appendChild(newItem);
+
+console.log(newItem.nextSibling);
